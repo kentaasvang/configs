2d0226351cd7266de49e58900353cf28794c939c
Kent Martin Åsvang <github.com@asvang.no>
2024-03-18 20:11:10 +0100

    added install scripts

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
+HOME_FOLDER="home"
 
-cp ./configs/** ~/
+
+# Copy all config files
+if [ -d HOME_FOLDER ]
+then
+	echo "Copying files to home folder.."
+	cp -R ./HOME_FOLDER/** ~/
+else
+	echo "home folder doesn't exist"
+fi
+
+echo "Done installing files"
+
