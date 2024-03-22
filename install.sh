82019324dd78e5b62b6f25e42eca2a69c6b2f0a3
Kent Martin Åsvang <github.com@asvang.no>
2024-03-22 19:12:16 +0100

    fixed install.sh and added fish as default shell in tmux config

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@ HOME_FOLDER="home"
 
 
 # Copy all config files
-if [ -d HOME_FOLDER ]
+if [ -d $HOME_FOLDER ]
 then
 	echo "Copying files to home folder.."
-	cp -R ./HOME_FOLDER/** ~/
+	cp -a ./$HOME_FOLDER/. ~/
 else
-	echo "home folder doesn't exist"
+	echo "'$HOME_FOLDER' folder doesn't exist"
 fi
 
 echo "Done installing files"
