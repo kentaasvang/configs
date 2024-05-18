e6c717a27ac6e796da42aabe51d6c411d312b9ef
Kent Martin Åsvang <github.com@asvang.no>
2024-05-18 17:50:16 +0200

    added tree-sitter

--- private_dot_config/nvim/lua/kasvang/vim-plug.lua
+++ private_dot_config/nvim/lua/kasvang/vim-plug.lua
@@ -4,6 +4,7 @@ vim.cmd [[
 call plug#begin('~/.local/share/nvim/plugged')
 
 	Plug 'neoclide/coc.nvim', {'branch': 'release'}
+	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 
 call plug#end()
 ]]
