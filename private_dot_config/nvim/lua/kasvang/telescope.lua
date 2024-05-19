41d3ba469709da75f8d43f1f444bb60f6d1dfa5e
Kent Martin Åsvang <github.com@asvang.no>
2024-05-19 12:35:05 +0200

    added <C-p> to search git-files (don't want all files in venv/ node_modules etc

--- private_dot_config/nvim/lua/kasvang/telescope.lua
+++ private_dot_config/nvim/lua/kasvang/telescope.lua
@@ -1,5 +1,10 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', function()
 	builtin.find_files({previewer = false })
+end, {})
+
+vim.keymap.set('n', '<C-p>', function()
+	builtin.git_files({previewer = false })
 end, {})
 
