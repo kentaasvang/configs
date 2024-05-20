2bcea0c625e69fcebe801a0f2329fec10d705778
Kent Martin Åsvang <github.com@asvang.no>
2024-05-20 10:20:10 +0200

    added digagnostic tooling and fixed <Tab> again

--- private_dot_config/nvim/lua/kasvang/remap.lua
+++ private_dot_config/nvim/lua/kasvang/remap.lua
@@ -1,33 +1,59 @@
 vim.g.mapleader = ' '  -- Space as leader key
 vim.g.maplocalleader = ' '  -- Space as local leader key
 
--- Key mappings
---vim.api.nvim_set_keymap('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_backspace() ? "<TAB>" : coc#refresh()', {expr = true, noremap = true, silent = true})
---vim.api.nvim_set_keymap('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"', {expr = true, noremap = true, silent = true})
---vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"', {expr = true, noremap = true, silent = true})
---vim.api.nvim_set_keymap('i', '<C-Space>', 'coc#refresh()', {expr = true, noremap = true, silent = true})
+local keyset = vim.keymap.set
 
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {silent = true})
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {silent = true})
-vim.api.nvim_set_keymap('n', 'K', ':lua show_documentation()<CR>', {noremap = true, silent = true})
+-- Autocomplete
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
 
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+                
+-- GoTo code navigation
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+    
 vim.api.nvim_set_keymap('v', '<C-c>', '"+y', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-c>', '"+y', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-v>', '"+p', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', {noremap = true, silent = true})
+
+-- Use `[g` and `]g` to navigate diagnostics
+-- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
+keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+keyset("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
 
 -- Functions
 function _G.check_backspace()
   local col = vim.fn.col('.') - 1
   return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
 end
+    
+-- Symbol renaming
+keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
 
-function _G.show_documentation()
-  if vim.fn['CocAction']('hasProvider', 'hover') then
-    vim.fn['CocActionAsync']('doHover')
-  else
-    vim.fn.feedkeys('K', 'in')
-  end
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
 end
+
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+    
