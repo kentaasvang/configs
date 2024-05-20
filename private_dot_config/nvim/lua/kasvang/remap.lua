6530212314ae5731ee1f135a1ad29a059a938f44
Kent Martin Åsvang <github.com@asvang.no>
2024-05-20 09:52:53 +0200

    removed weird side-effect on tabs and sat tab-length to 4 spaces

--- private_dot_config/nvim/lua/kasvang/remap.lua
+++ private_dot_config/nvim/lua/kasvang/remap.lua
@@ -2,10 +2,10 @@ vim.g.mapleader = ' '  -- Space as leader key
 vim.g.maplocalleader = ' '  -- Space as local leader key
 
 -- Key mappings
-vim.api.nvim_set_keymap('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_backspace() ? "<TAB>" : coc#refresh()', {expr = true, noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"', {expr = true, noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"', {expr = true, noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', '<C-Space>', 'coc#refresh()', {expr = true, noremap = true, silent = true})
+--vim.api.nvim_set_keymap('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_backspace() ? "<TAB>" : coc#refresh()', {expr = true, noremap = true, silent = true})
+--vim.api.nvim_set_keymap('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"', {expr = true, noremap = true, silent = true})
+--vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"', {expr = true, noremap = true, silent = true})
+--vim.api.nvim_set_keymap('i', '<C-Space>', 'coc#refresh()', {expr = true, noremap = true, silent = true})
 
 vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {silent = true})
 vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
--- private_dot_config/nvim/lua/kasvang/settings.lua
+++ private_dot_config/nvim/lua/kasvang/settings.lua
@@ -9,3 +9,9 @@ vim.o.updatetime = 300
 vim.wo.signcolumn = 'yes'
 vim.wo.wrap = false
 vim.opt.guicursor = ""
+                
+-- Tab
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
