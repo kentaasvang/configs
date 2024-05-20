a13836cd7daf1127634a71cdaaf7f50920a271ce
Kent Martin Åsvang <github.com@asvang.no>
2024-05-20 11:06:00 +0200

    <cr> to accept current selected completion item

--- private_dot_config/nvim/lua/kasvang/remap.lua
+++ private_dot_config/nvim/lua/kasvang/remap.lua
@@ -17,6 +17,10 @@ end
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
                 
 -- GoTo code navigation
 keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
