3a22fbf223e9ab67eef376b22fb98e07097c029d
Kent Martin Åsvang <github.com@asvang.no>
2024-10-19 10:51:01 +0200

    commented out copilot

--- private_dot_config/nvim/lua/kasvang/vim-plug.lua
+++ private_dot_config/nvim/lua/kasvang/vim-plug.lua
@@ -5,10 +5,11 @@ vim.fn["plug#"]("neoclide/coc.nvim", { branch = "release" })
 vim.fn["plug#"]("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
 vim.fn["plug#"]("nvim-lua/plenary.nvim")
 vim.fn["plug#"]("nvim-telescope/telescope.nvim", { tag = "0.1.6" })
+vim.fn["plug#"]("rust-lang/rust.vim")
 
 vim.fn["plug#"]("rose-pine/neovim", { as = "rose-pine" })
 vim.fn["plug#"]("lepture/vim-jinja")
-vim.fn["plug#"]("github/copilot.vim")
+# vim.fn["plug#"]("github/copilot.vim")
 
 vim.call("plug#end")
 
